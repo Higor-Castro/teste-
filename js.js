ee83a46ae33b7859acdbef8c1be50e955e211da7
Higor de Castro Venâncio da Silva <112348220+Higor-Castro@users.noreply.github.com>
2026-05-12 14:00:30 -0300

    update

--- js.js
+++ js.js
@@ -2,28 +2,36 @@ window.addEventListener('load', () => {
 
     const primeiraCarga = sessionStorage.getItem('primeiraCarga');
 
-    if (!primeiraCarga) {
-        sessionStorage.setItem('primeiraCarga', 'false');
+    if (primeiraCarga === null) {
+
+        sessionStorage.setItem('primeiraCarga', '1');
+
         return;
     }
+
     Flutter.postMessage("getMessage");
+
 });
 
 function reload() {
+
     location.reload();
+
 }
 
 function getMessage(data) {
+
     const title = data?.title ?? '';
-    if (title.includes("Tela 1")){
+
+    if (title.includes("Tela 1")) {
+
         window.location.href = "tela1.html";
+
     }
-    else if (title.includes("Tela 2")){
+    else if (title.includes("Tela 2")) {
+
         window.location.href = "tela2.html";
+
     }
-    else{
-        return;
-    }
-    
 
 }
